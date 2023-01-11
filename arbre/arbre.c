7625328b732ad8abf6ce9d9d7e4f1ccdd55c3cfc
iMoubarak <imoubarak862@gmail.com>
2023-01-11 23:20:37 +0100

    ci (arbre) : creation de la structure arbre et implementation des fonctions

---
+++ arbre/arbre.c
@@ -0,0 +1,128 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "arbre.h"
+arbre *nouveauA()
+{
+    arbre *b;
+    b = NULL;
+    return b;
+}
+int EstArbreVide(arbre* b)
+{
+    return (b==NULL);
+}
+void AffichagePrefixe(arbre *b)
+{
+    if(!EstArbreVide(b))
+    {
+        printf("[%d]\t",b->donnee);
+        AffichagePrefixe(b->g);
+        AffichagePrefixe(b->d);
+    }
+}
+void AffichageInfixe(arbre *b)
+{
+    if(!EstArbreVide(b))
+    {
+        AffichageInfixe(b->g);
+        printf("[%d]\t",b->donnee);
+        AffichageInfixe(b->d);
+    }
+} 
+void AffichagePostfixe(arbre* b)
+{
+    if(!EstArbreVide(b))
+    {
+        AffichagePostfixe(b->g);
+        AffichagePostfixe(b->d);
+        printf("[%d]\t",b->donnee);
+    }
+}
+arbre *InsereRacineD(arbre* b,int donnee)
+{
+    arbre *a = (arbre*)malloc(sizeof(arbre));
+    a->donnee = donnee;
+    a->g = NULL;
+    if(b==NULL)
+    {
+        a->d = NULL;
+        b = a;
+    }
+    else
+    {
+        a->d = b;
+        b = a;
+    }
+    return b;
+}
+arbre *InsereRacineG(arbre* b,int donnee)
+{
+    arbre *a = (arbre*)malloc(sizeof(arbre));
+    a->donnee = donnee;
+    a->d = NULL;
+    if(b==NULL)
+    {
+        a->g = NULL;
+        b = a;
+    }
+    else
+    {
+        a->g = b;
+        b = a;
+    }
+    return b;
+}
+arbre *InsereFeuilleD(arbre* b,int donnee)
+{
+    arbre *a = (arbre*)malloc(sizeof(arbre));
+    arbre *ptr;
+    a->donnee = donnee;
+    a->g = NULL;
+    a->d = NULL;
+    if(EstArbreVide(b))
+        b = a;
+    else
+    {
+        ptr = b;
+        while(ptr->d!=NULL)
+            ptr = ptr->d;
+        ptr->d = a;
+    }
+    return b;
+}
+arbre *InsereFeuilleG(arbre* b,int donnee)
+{
+    arbre *a = (arbre*)malloc(sizeof(arbre));
+    arbre *ptr;
+    a->donnee = donnee;
+    a->g = NULL;
+    a->d = NULL;
+    if(EstArbreVide(b))
+        b = a;
+    else
+    {
+        ptr = b;
+        while(ptr->g!=NULL)
+            ptr = ptr->g;
+        ptr->g = a;
+    }
+    return b;
+}
+int SommeElement(arbre* b)
+{
+    if(b==NULL)
+        printf("\nArbre vide");
+    else
+        return b->donnee;
+}
+int MaxElement(arbre* b)
+{
+    if(!EstArbreVide(b))
+    {
+        
+    }
+}
+int MinElement(arbre* b)
+{
+
+}
---
+++ arbre/arbre.h
@@ -0,0 +1,22 @@
+#ifndef __ARBRE__
+#define __ARBRE__
+typedef struct arbre
+{
+    int donnee;
+    struct arbre *d;
+    struct arbre *g;
+}arbre;
+arbre *nouveauA();
+int EstArbreVide(arbre*);
+void AffichagePrefixe(arbre*); // On visite :  Rprincipale SAgauche SAdroite
+void AffichageInfixe(arbre*); // On visite SAgauche Rprincipale SAdroite
+void AffichagePostfixe(arbre*); // On visite : SAgauche SAdroite RPrincipale
+arbre *InsereRacineD(arbre*,int);
+arbre *InsereRacineG(arbre*,int);
+arbre *InsereFeuilleD(arbre*,int);
+arbre *InsereFeuilleG(arbre*,int);
+int SommeElement(arbre*);
+int MaxElement(arbre*);
+int MinElement(arbre*);
+
+#endif
